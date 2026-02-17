fe00333d4be6855a234625213a24df996c0e2bed
siva <siva@git.com>
2026-02-17 18:31:21 +0530

    Updated for Render deployment

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend Connected Successfully!" });
 });
 
-const PORT = 5000;
+//const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
